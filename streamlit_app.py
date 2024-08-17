12782a1656e0d3ce9f5bad5fe2b49c6a0afec624
theomdvn <theomida@gmail.com>
2024-08-17 21:29:23 +0200

    fixing navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-stock_screeners = st.Page("Screeners/Stocks_Screeners.py", title="Stocks_Screeners", icon=":material/search:")
-options = st.Page("Visualization/Options_strat.py", title="Options_Strat", icon=":material/history:")
+options = st.Page("Visualization\Options_strat.py", title="Options_Strat", icon=":material/history:",default=True)
+stock_screeners = st.Page("Screeners\Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
+
 
 pg = st.navigation(
         {
             "Screeners": [stock_screeners],
             "Visualization": [options],
-        })
+        },
+        position="sidebar")
 pg.run()
