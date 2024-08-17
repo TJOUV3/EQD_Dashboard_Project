7bb0b2b4f6608b0d52f37c11727d86e98b94e4ea
TJOUV3 <t.jouve@yahoo.com>
2024-08-17 23:11:01 +0200

    renamed:    config.toml -> .streamlit/config.toml 	new file:   Tasks/TodoList.py 	modified:   streamlit_app.py

---
+++ Tasks/TodoList.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write('Filtre actif pour options US')
+st.write('restylisé Heatmap')
+st.write('Compléter S&P 500 Stocks Screener tab avec des nouvelles colonnes')
+st.write("ajouter données dans Options strat à la place de X et Y (graph prix ....), restyliser l'ajout d'options avec les bonnes couleurs")
+st.write('repenser le graph option price')
+st.write('penser au hedging, faire pour chaque grecque, possibilité de rentrer les niveaux de grecques souhaités et proposition de montage')
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,14 @@ import streamlit as st
 options = st.Page("Visualization\Options_strat.py", title="Options_Strat", icon=":material/history:",default=True)
 stock_screeners = st.Page("Screeners\Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
 heatmap = st.Page("Screeners\Heatmap.py", title="Heatmap", icon=":material/square:")
+tasks = st.Page("Tasks\TodoList.py", title="TodoList", icon=":material/square:")
 
 
 pg = st.navigation(
         {
             "Screeners": [stock_screeners,heatmap],
             "Visualization": [options],
+            "Next Tasks" : [tasks]
         },
         position="sidebar")
 
