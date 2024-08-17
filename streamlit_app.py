260be5206e3eece72b24ee0ae7d89b59e492deca
TJOUV3 <t.jouve@yahoo.com>
2024-08-17 21:34:42 +0200

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-options = st.Page("Visualization\Options_strat.py", title="Options_Strat", icon=":material/history:",default=True)
+options = st.Page("Visualization\Options_strat.py", title="Options_Strat", icon=":material/history:", default=True)
 stock_screeners = st.Page("Screeners\Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
 
 
