7b397fa2295d688f54e94e1b1291720935504e30
theomdvn <theomida@gmail.com>
2024-08-17 22:32:45 +0200

    change icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 options = st.Page("Visualization\Options_strat.py", title="Options_Strat", icon=":material/history:",default=True)
 stock_screeners = st.Page("Screeners\Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
-heatmap = st.Page("Screeners\Heatmap.py", title="Stocks_Screeners", icon=":material/search:")
+heatmap = st.Page("Screeners\Heatmap.py", title="Heatmap", icon=":material/square:")
 
 
 pg = st.navigation(
