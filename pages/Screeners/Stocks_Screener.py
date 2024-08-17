da2a953e90b7cd5bc79a32a32a480bff19dbc616
TJOUV3 <t.jouve@yahoo.com>
2024-08-17 21:30:56 +0200

    demi update

--- pages/Screeners/Stocks_Screener.py
+++ pages/Screeners/Stocks_Screener.py
@@ -1,2 +1,5 @@
 import streamlit as st
 
+def show():
+    st.write("Welcome to the Stocks Screeners Page")
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-stock_screeners = st.Page("Screeners/Stocks_Screeners.py", title="Stocks_Screeners", icon=":material/search:")
-options = st.Page("Visualization/Options_strat.py", title="Options_Strat", icon=":material/history:")
+stock_screeners = st.Page("pages/Screeners/Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
+options = st.Page("pages/Visualization/Options_strat.py", title="Options_Strat", icon=":material/history:", default=True)
 
 pg = st.navigation(
         {
