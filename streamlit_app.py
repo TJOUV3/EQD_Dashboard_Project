b07a76314d9126ed9a803fdeb40e23e2bcb5e090
TJOUV3 <t.jouve@yahoo.com>
2024-08-17 21:45:04 +0200

    modified:   .DS_Store 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-options = st.Page("Visualization\Options_strat.py", title="Options_Strat", icon=":material/history:", default=True)
-stock_screeners = st.Page("Screeners\Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
+options = st.Page("Visualization\Options_strat.py", title="Options_Strat", icon=":material/history:", default=False)
+stock_screeners = st.Page("Screeners\Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:", default=True)
 
 
 pg = st.navigation(
