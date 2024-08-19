47d50396f66bebe3e019ad9699493a1f290376d3
TJOUV3 <t.jouve@yahoo.com>
2024-08-19 23:40:28 +0200

    modified:   Tasks/TodoList.py

--- Tasks/TodoList.py
+++ Tasks/TodoList.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.write('Filtre actif pour options US')
-st.write('restylisé Heatmap TREEMAP BOUFFON')
 st.write('Compléter S&P 500 Stocks Screener tab avec des nouvelles colonnes')
 st.write("ajouter données dans Options strat à la place de X et Y (graph prix ....), restyliser l'ajout d'options avec les bonnes couleurs")
 st.write('repenser le graph option price')
