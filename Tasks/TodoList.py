10c0b8ef56d63ead58d9b3da6e3da264e6d57636
theomdvn <theomida@gmail.com>
2024-08-19 23:52:45 +0200

    Todo

--- Tasks/TodoList.py
+++ Tasks/TodoList.py
@@ -6,3 +6,10 @@ st.write("ajouter données dans Options strat à la place de X et Y (graph prix 
 st.write('repenser le graph option price')
 st.write('penser au hedging, faire pour chaque grecque, possibilité de rentrer les niveaux de grecques souhaités et proposition de montage')
 
+st.subheader('Hedging')
+
+st.write('On choisit 1 produit casi pur par grecque, ex delta == stock')
+st.write('Vega == Straddle')
+st.write('Gamma == opt atm + short term')
+st.write('Theta == impossible put > call')
+st.write("Rho == maturité élevée ")
