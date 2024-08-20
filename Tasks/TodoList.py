fb321040ece9d7abec78377d822e2b57d5540edf
TJOUV3 <t.jouve@yahoo.com>
2024-08-20 18:29:56 +0200

    modified:   Tasks/TodoList.py

--- Tasks/TodoList.py
+++ Tasks/TodoList.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write('Filtre actif pour options US')
 st.write('Compléter S&P 500 Stocks Screener tab avec des nouvelles colonnes')
-st.write("ajouter données dans Options strat à la place de X et Y (graph prix ....), restyliser l'ajout d'options avec les bonnes couleurs")
+st.write("ajouter données dans Options strat à la place de X et Y (graph prix ....), restyliser l'ajout d'options avec les bonnes couleurs, en X mettre la valeur de Delta, Y Gamma etc etc ")
 st.write('repenser le graph option price')
 st.write('penser au hedging, faire pour chaque grecque, possibilité de rentrer les niveaux de grecques souhaités et proposition de montage')
 
