9d19ba1810ad7548f4439359f8f1e3506add9b94
theomdvn <theomida@gmail.com>
2025-03-16 23:24:31 +0100

    V1.0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-options = st.Page("Visualization/Options_strat.py", title="Options_Strat", icon=":material/history:",default=True)
+# options = st.Page("Visualization/Options_strat.py", title="Options_Strat", icon=":material/history:",default=True)
 options_BS = st.Page("Visualization/BS_Options.py", title="Options_BS", icon=":material/history:")
 
-options_vantage = st.Page("Visualization/Options.py", title="Options", icon=":material/history:")
-volatility_surface = st.Page("Visualization/Volatility_surface.py", title="Volatility Surface", icon=":material/history:")
-heston = st.Page("Visualization/Heston.py", title="Heston", icon=":material/history:")
-test_heston = st.Page("Visualization/Prepa_Heston.py", title="Prepa Heston", icon=":material/history:")
+# options_vantage = st.Page("Visualization/Options.py", title="Options", icon=":material/history:")
+# volatility_surface = st.Page("Visualization/Volatility_surface.py", title="Volatility Surface", icon=":material/history:")
+# heston = st.Page("Visualization/Heston.py", title="Heston", icon=":material/history:")
+# test_heston = st.Page("Visualization/Prepa_Heston.py", title="Prepa Heston", icon=":material/history:")
 
-stock_screeners = st.Page("Screeners/Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
-heatmap = st.Page("Screeners/Heatmap.py", title="Heatmap", icon=":material/square:")
-tasks = st.Page("Tasks/TodoList.py", title="TodoList", icon=":material/square:")
+# stock_screeners = st.Page("Screeners/Stocks_Screener.py", title="Stocks_Screeners", icon=":material/search:")
+# heatmap = st.Page("Screeners/Heatmap.py", title="Heatmap", icon=":material/square:")
+# tasks = st.Page("Tasks/TodoList.py", title="TodoList", icon=":material/square:")
 
 
 pg = st.navigation(
